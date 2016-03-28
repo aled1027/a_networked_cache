67121de71d4dafeb0efd598b2f7de7bec178be6a
aled1027 <a.led1027@gmail.com>
2016-03-27 17:33:05 -0700

    added easy testing and shutdown command for server

--- include/network_server.h
+++ include/network_server.h
@@ -24,7 +24,7 @@ class NetworkServer {
 
         std::string receive() {
             // receives a message. end of message is signified by "\n"
-            debug("NetworkServer::receive");
+            //debug("NetworkServer::receive");
             boost::asio::streambuf sb;
             boost::system::error_code ec;
             boost::asio::read_until(socket, sb, "\n", ec);
@@ -59,34 +59,48 @@ class HTTPServer {
         void listen() {
             debug("HTTPServer::listen");
 
+            std::string raw_request("");
             while (true) {
                 // loops until a shutdown request.
                 // wait for request
-                std::string raw_request;
                 do {
                     raw_request = network_server.receive();
                 } while (raw_request.compare("") == 0);
 
                 // process request
-                process_request(parse_request(raw_request));
+                bool shutdown = process_request(raw_request);
+                raw_request = "";
+
+                if (shutdown)
+                    break;
             }
         }
 
-        void process_request(http_info hi) {
+        bool process_request(std::string raw_request) {
+            // return true if should shutdown else returns false
+            http_info request = parse_request(raw_request);
+            
             debug("HTTPServer::process_request");
 
-            if (hi.method.compare("GET")) {
-            } else if (hi.method.compare("POST")) {
-            } else if (hi.method.compare("PUT")) {
-            } else if (hi.method.compare("DELETE")) {
-            } else if (hi.method.compare("HEAD")) {
+            if (request.method.compare("GET") == 0) {
+            } else if (request.method.compare("POST") == 0) {
+                if (request.path.compare("/shutdown") == 0) {
+                    network_server.send("HTTP/1.1 200 OK");
+                    return true;
+                }
+
+            } else if (request.method.compare("PUT") == 0) {
+            } else if (request.method.compare("DELETE") == 0) {
+            } else if (request.method.compare("HEAD") == 0) {
             } else {
-                network_server.send("HTTP/1.0 400 Bad Request\n");
+                network_server.send("HTTP/1.1 400 Bad Request\n");
             }
 
-            std::string response = "HTTP/1.0 400 Bad Request\n";
+            std::string response = "HTTP/1.1 400 Bad Request\n";
             network_server.send(response);
+            return false;
         }
+
 
         
 };
--- src/network_tests.cpp
+++ src/network_tests.cpp
@@ -5,21 +5,34 @@
 #include "network_tests.h"
 
 
-void client_test() {
-    HTTPClient c;
-    c.request("GET", "/3", "version");
+void test_shutdown(bool is_client) {
+    std::cout << "Running shutdown test" << std::endl;
+    if (is_client) {
+        HTTPClient c;
+        c.request("POST", "/shutdown", "HTTP/1.1");
+    } else {
+        HTTPServer s;
+        s.listen();
+    }
 }
 
-void server_test() {
-    HTTPServer s;
-    s.listen();
+
+void client_tests() { 
+    bool is_client = true;
+    test_shutdown(is_client);
 }
+
+void server_tests() { 
+    bool is_client = false;
+    test_shutdown(is_client);
+}
+
 
 void network_tests(std::string user) {
     std::cout << "Running network_tests" << std::endl;
     if (user.compare("client") == 0) {
-        client_test();
+        client_tests();
     } else {
-        server_test();
+        server_tests();
     }
 }
