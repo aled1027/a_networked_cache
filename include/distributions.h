2f2e55d4f8021692205a3c1b0b9c739647fa7493
aled1027 <a.led1027@gmail.com>
2016-04-11 14:00:14 -0700

    added distributions

---
+++ include/distributions.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Taken from the mutilate project: https://github.com/leverich/mutilate
+// If you change this, make sure to update distributions.cc.
+
+enum distribution_t { UNIFORM, EXPONENTIAL, ZIPFIAN, LATEST };
+extern const char* distributions[];
+
+double generate_normal(double mean, double sd);
+double generate_poisson(double lambda);
+double generate_uniform(double lambda);
+
+distribution_t get_distribution(const char *name);
